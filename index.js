fce4769ee3ac3de3f86632a85feb04043d35db46
olimp-50 <olimp-50@ttit.local>
2025-03-11 13:52:32 +0700

    grid fix, inputs

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ $ = (id) => document.querySelector(id)
 
 window.onload = () => {
     $('#header__burger_button').addEventListener('click', function(){
-        $('main').style.cssText = "display: none"
+        if($("#burger-hidden")){
+            $("#burger-hidden").id = 'burger-show'
+            $("main").style.display = 'none'
+            $(".footer").style.display = 'none'
+        } else {
+            $("#burger-show").id = 'burger-hidden'
+            $("main").style.display = 'block'
+            $(".footer").style.display = "block"
+        }
     })
 }
